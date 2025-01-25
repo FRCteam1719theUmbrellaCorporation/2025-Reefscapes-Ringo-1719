6beb8361baf0ed61ca7adee9e10944743b0c09fa
Neel Shrestha <nepalimacha@gmail.com>
2025-01-25 15:29:05 -0500

    changed speed limiter to var

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,7 +22,10 @@ public final class Constants
   public static final double ROBOT_MASS = (148 - 20.3) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(14.5/4);
+  //IMB: We verified that this is the currect way to limit the max speed in YAGSL
+  public static final double SPEED_LIMITER  = 0.25;
+  public static final double MAX_SPEED  = Units.feetToMeters(14.5*SPEED_LIMITER);
+  //This new max speed multiplied by the speed limiter should be the best max speed
   // Maximum speed of the robot in meters per second, used to limit acceleration.
 
 //  public static final class AutonConstants

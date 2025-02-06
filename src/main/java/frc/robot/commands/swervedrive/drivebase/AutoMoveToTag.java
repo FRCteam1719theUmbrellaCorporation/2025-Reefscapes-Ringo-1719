40e883a8dbe3b31b069112f6b916a7444a331274
Ianborden <ianb27@parkschool.net.com>
2025-02-06 13:47:01 -0500

    Fully writen copy of Team 7028's movetotag code

--- src/main/java/frc/robot/commands/swervedrive/drivebase/AutoMoveToTag.java
+++ src/main/java/frc/robot/commands/swervedrive/drivebase/AutoMoveToTag.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -35,7 +36,7 @@ public class AutoMoveToTag extends Command {
   private final Supplier<Pose2d> poseProvider;
 
   //In the place of a, b, and c put PIDS
-  private final ProfiledPIDController xController = new ProfiledPIDController(a,b ,c, X_CONSTRAINTS);
+  private final ProfiledPIDController xController = new ProfiledPIDController(a,b,c, X_CONSTRAINTS);
   private final ProfiledPIDController yController = new ProfiledPIDController(a,b ,c, Y_CONSTRAINTS);
   private final ProfiledPIDController omegaController = new ProfiledPIDController(a,b ,c, OMEGA_CONSTRAINTS);
 
@@ -56,25 +57,93 @@ public class AutoMoveToTag extends Command {
     yController.setTolerance(0.2);
     omegaController.setTolerance(Units.degreesToRadians(3));
     omegaController.enableContinuousInput(-Math.PI, Math.PI);
+
+    addRequirements(drivetrainSubsystem);
   
   }
+  //they didnt have this class
 
   /** Creates a new AutoMoveToTag. */
-  public AutoMoveToTag() {
+  //public AutoMoveToTag() {
     // Use addRequirements() here to declare subsystem dependencies.
-  }
+  //}
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    lastTarget = null;
+    var robotPose = poseProvider.get();
+    omegaController.reset(robotPose.getRotation().getRadians());
+    xController.reset(robotPose.getX());
+    yController.reset(robotPose.getY());
+
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    var robotPose2d = poseProvider.get();
+    var robotPose = 
+        new Pose3d(
+            robotPose2d.getX(),
+            robotPose2d.getY(),
+            z:0.0,
+            new Rotation3d(0.0,0.0,robotPose2d.getRotation().getRadians())
+            //Ian Borden doesn't know how to end the line above as the video didn't show the full line
+            )
+    
+    var photonRes = photonCamera.getLatestResult();
+    if (photonRes.hasTargets()){
+      var targetOpt = photonRes.getTargets().stream()
+          .filter(t -> t.getFiducialId()==TAG_TO_CHASE)
+          .filter(t -> !t.equals(lastTarget) && t.getPoseAmbiguity() <=0.2 && t.getPose)
+          //the line above is incomplete as the full line was not shown in the video
+          .findFirst();
+      if(targetOpt.isPresent()){
+        var target = targetOpt.get();
+        lastTarget = target;
+
+        var cameraPose = robotPose.transformBy(ROBOT_TO_CAMERA);
+
+        var camToTarget = target.getBestCameraToTarget();
+        var targetPose = cameraPose.transformBy(camToTarget);
+
+        var goalPose = targetPose.transformBy(TAG_TO_GOAL).toPose2d();
+
+        xController.setGoal(goalPose.getX());
+        yController.setGoal(goalPose.getY());
+        omegaController.setGoal(goalPose.getRotation().getRadians());
+      }
+      
+
+    }
+
+    if (lastTarget == null){
+      drivetrainSubsystem.stop();
+    } else{
+      var xSpeed = xController.calculate(robotPose.getX());
+      if(xController.atGoal()){
+        xSpeed = 0;
+      }
+      var ySpeed = yController.calculate(robotPose.getY());
+      if (yController.atGoal()){
+        ySpeed = 0;
+      }
+      var omegaSpeed = omegaController.calculate(robotPose2d.getRotation().getRadians());
+      if (omegaController.atGoal()){
+        omegaSpeed = 0;
+      }
+
+      drivetrainSubsystem.drive(
+        ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed,omegaSpeed, robotPose2d.get(/*get something*/)))
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    drivetrainSubsystem.stop;
+  }
 
   // Returns true when the command should end.
   @Override
